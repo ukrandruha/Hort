2f634aeed91a2274598515f5bed33e1d7dbdbae9
ukrandruha <ukrandruha@gmail.com>
2025-12-13 10:08:00 +0200

    route api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ const app = Fastify();
    });
 
    await app.register(authRoutes);
-   await app.register(robotRoutes);
+   await app.register(robotRoutes, { prefix: "/api" });
 
    app.listen({ port: 3000 }, () => console.log("Backend started"));
 })();

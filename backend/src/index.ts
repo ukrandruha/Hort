036261e51ff627d723170325b91fd259ae0e2e15
ukrandruha <ukrandruha@gmail.com>
2025-12-14 09:08:41 +0200

    index fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,25 +3,37 @@ import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
 import { PrismaClient } from "@prisma/client";
 
-
 import { robotRoutes } from "./robots/robot.routes.js";
 import { authRoutes } from "./auth/auth.routes.js";
 
 const prisma = new PrismaClient();
+
 const app = Fastify({
   logger: true
 });
 
-(async () => {
-   await app.register(cors, { origin: "*" });
-   await app.register(jwt, { secret: "HORT_SECRET_KEY" });
+const start = async () => {
+  try {
+    await app.register(cors, { origin: "*" });
+    await app.register(jwt, { secret: "HORT_SECRET_KEY" });
 
-   app.decorate("auth", async (req: any, reply: any) => {
+    app.decorate("auth", async (req: any, reply: any) => {
       await req.jwtVerify();
-   });
+    });
 
-   await app.register(authRoutes);
-   await app.register(robotRoutes);
+    await app.register(authRoutes);
+    await app.register(robotRoutes);
 
-   app.listen({ port: 3000 }, () => console.log("Backend started"));
-})();
+    await app.listen({
+      port: 3000,
+      host: "0.0.0.0"
+    });
+
+    console.log("Backend started");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();

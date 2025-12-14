53d695f4492c23991fa2f6c74e61ef753d131164
ukrandruha <ukrandruha@gmail.com>
2025-12-14 09:33:25 +0200

    port

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://andrii.razoom-print.com"
+  baseURL: "http://localhost:3001"
 });
 
 api.interceptors.request.use((config) => {

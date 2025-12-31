248d068655aa07ae11fdc0fea5dce318a6078315
ukrandruha <ukrandruha@gmail.com>
2025-12-31 09:46:38 +0200

    robotId

--- backend/src/robots/robotCamera.routes.ts
+++ backend/src/robots/robotCamera.routes.ts
@@ -7,9 +7,9 @@ import {
 
 export async function robotCameraRoutes(app: FastifyInstance) {
 
- app.post("/api/robots/cameras:robotId", async (req, reply) => {
+ app.post<{ Params: { robotId: string } }>("/api/robots/cameras:robotId", async (req, reply) => {
      
-      const { robotId } = req.params as any;;
+      const { robotId } = req.params.robotId;
       const cameras = req.body as any[];
 
       await syncRobotCameras(robotId, cameras);
